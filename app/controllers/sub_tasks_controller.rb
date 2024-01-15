86909a4a13148e4e1dd0ef632b31a7067a284f4c
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-01-15 17:56:04 +0900

    Update sub_tasks_controller.rb

--- app/controllers/sub_tasks_controller.rb
+++ app/controllers/sub_tasks_controller.rb
@@ -1,2 +1,22 @@
 class SubTasksController < ApplicationController
+  before_action :set_task
+  before_action :set_sub_task, only: [:show, :destroy]
+
+  def show
+  end
+
+  def destroy
+    @sub_task.destroy
+    redirect_to task_path(@task), notice: 'Sub-task was successfully destroyed.'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def set_sub_task
+    @sub_task = @task.sub_tasks.find(params[:id])
+  end
 end
