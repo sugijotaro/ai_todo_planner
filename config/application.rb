bfa44fe14216c429d340d8290962950084bb18c5
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-01-18 00:52:38 +0900

    デプロイ

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module AiTodoPlanner
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'ai-todo-planner.onrender.com'
   end
 end
