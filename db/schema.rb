197328746e43c008ada8ca32e8ac6e72f6bc83ca
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-01-18 01:31:11 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_17_044821) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sub_tasks", force: :cascade do |t|
-    t.integer "task_id", null: false
+    t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "content"
