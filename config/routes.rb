cdae2dea17ab737ea9a996f899eea49905888fa7
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-01-18 01:39:32 +0900

    Render

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'render/index'
+  get "up" => "rails/health#show", as: :rails_health_check
+  root "render#index"
+
   resources :tasks do
     member do
       put :complete
